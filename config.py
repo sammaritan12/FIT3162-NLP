e550f37b6cedff75d740ff6b7582b40febd64ed0
Mark Patricio <mark.patricio@hotmail.com>
2018-08-17 13:07:39 +1000

    Reports, Reflections and Rerun the classifier

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # TWEAK CLASSIFIER OPTIONS
 ngram_length = 4  # length of character ngrams
-ngram_common_words = 5  # how many of the most common ngrams to choose as feature sets  
+ngram_common_words = 1000  # how many of the most common ngrams to choose as feature sets  
 normalization_type = 'l1' # approacah for normalisation
     # L1 Least Absolute Deviations, abs(sum of row) = 1, insensitive to outliers
     # L2 Least Squares, sum of squares, on each row = 1, takes outliers into consideration
