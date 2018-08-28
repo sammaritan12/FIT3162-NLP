ab148575bef8258efc60712fa2c44a840e5dbc8c
Mark Patricio <mark.patricio@hotmail.com>
2018-08-28 12:52:00 +1000

    Preprocessed more English Texts
    50 more texts are preprocessed, reworked preprocessor.py so I can work with entire folders

--- ProcessedTexts/preprocessor.py
+++ ProcessedTexts/preprocessor.py
@@ -1,29 +1,54 @@
 from timeit import default_timer as timer
+from glob import glob
+from sys import argv
+import os
 
-filename = input("Enter filename: ")
-start = timer()
-file = open(filename, encoding='utf-8')
-fileLines = []
-i = 0
-for line in file:
-    if line.strip() != "":
-        fileLines.append(line.strip())
-    i += 1
-startFound = False
-startLine = 0
-for i in range(len(fileLines)):
-    if fileLines[i].startswith("***"):
-        if not startFound:
-            startLine = i
-            startFound = True
-        else:
-            endLine = i-1
-            break
-fileLines = fileLines[startLine+1:endLine]
-file.close()
-file = open(filename, "w")
-for line in fileLines:
-    file.write(line + "\n")
-file.close()
-end = timer()
-print("File processed in " + str((end-start)/1000) + " seconds")
+def list_filenames(dirpath):
+    return glob(dirpath + '/**/*.txt', recursive=True)
+
+def extract_text(filename):
+    start = timer()
+    file = open(filename, encoding='utf-8', errors='ignore')
+    fileLines = []
+    i = 0
+    for line in file:
+        if line.strip() != "":
+            fileLines.append(line.strip())
+        i += 1
+    startFound = False
+    startLine = 0
+    endLine = -1
+    for i in range(len(fileLines)):
+        if fileLines[i].startswith("***"):
+            if not startFound:
+                startLine = i
+                startFound = True
+            else:
+                endLine = i-1
+                break
+    fileLines = fileLines[startLine+1:endLine]
+    file.close()
+    file = open(filename, "w")
+    for line in fileLines:
+        file.write(line + "\n")
+    file.close()
+    end = timer()
+    print("File processed in " + str((end-start)/1000) + " seconds")
+
+def prepend_parent_dir_to_file(filename):
+    parent_dir_name = os.path.basename(os.path.dirname(filename))
+    with open(filename, "r+") as file:
+        content = file.read()
+        file.seek(0,0)
+        file.write(parent_dir_name.rstrip('\r\n') + '\n' + content)
+
+# Make sure there's an argument
+if len(argv) < 2:
+    print("Enter command as: preprocessor.py <directory_name>")
+    quit()
+
+# Gets all files in directory, processes and places author in it
+filenames = list_filenames(argv[1])
+for i in filenames:
+    extract_text(i)
+    prepend_parent_dir_to_file(i)
