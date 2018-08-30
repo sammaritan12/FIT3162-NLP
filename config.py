3013b966e66fdf575f085f0028b99f4c8e92e73c
Mark Patricio <mark.patricio@hotmail.com>
2018-08-30 12:06:17 +1000

    Forward slashes and cross validation testing for kernels
    Changed all backslashes to forward slashes to stop with string formatting troubles.
    
    Added two functions to test the kernels for the classifiers.

--- config.py
+++ config.py
@@ -7,26 +7,26 @@ normalization_type = 'l1' # approacah for normalisation
 # L2 Least Squares, sum of squares, on each row = 1, takes outliers into consideration
 
 # Paths for preprocessed texts
-en_processed_text_path = '.\processed_texts\english'
-sp_processed_text_path = '.\processed_texts\spanish'
+en_processed_text_path = './processed_texts/english'
+sp_processed_text_path = './processed_texts/spanish'
 
 # Paths for english classifiers and extracted char/word ngrams
-en_classifier_path = '.\serialised_objects\en_classifier.pkl'
-en_authors = '.\serialised_objects\en_authors.pkl'
-en_training_char_ngrams_path = '.\serialised_objects\en_training_char_ngrams.pkl'
-en_training_word_ngrams_path = '.\serialised_objects\en_training_word_ngrams.pkl'
-en_char_ngram_feature_set_path = '.\serialised_objects\en_char_ngram_feature_set.pkl'
-en_word_ngram_feature_set_path = '.\serialised_objects\en_word_ngram_feature_set.pkl'
-en_avg_sentence_length_feature_set_path = '.\serialised_objects\en_avg_sentence_length_feature_set.pkl'
+en_classifier_path = './serialised_objects/en_classifier.pkl'
+en_authors = './serialised_objects/en_authors.pkl'
+en_training_char_ngrams_path = './serialised_objects/en_training_char_ngrams.pkl'
+en_training_word_ngrams_path = './serialised_objects/en_training_word_ngrams.pkl'
+en_char_ngram_feature_set_path = './serialised_objects/en_char_ngram_feature_set.pkl'
+en_word_ngram_feature_set_path = './serialised_objects/en_word_ngram_feature_set.pkl'
+en_avg_sentence_length_feature_set_path = './serialised_objects/en_avg_sentence_length_feature_set.pkl'
 
 # Paths for english classifiers and extracted char/word ngrams
-sp_classifier_path = '.\serialised_objects\sp_classifier.pkl'
-sp_authors = '.\serialised_objects\sp_authors.pkl'
-sp_training_char_ngrams_path = '.\serialised_objects\sp_training_char_ngrams.pkl'
-sp_training_word_ngrams_path = '.\serialised_objects\sp_training_word_ngrams.pkl'
-sp_char_ngram_feature_set_path = '.\serialised_objects\sp_char_ngram_feature_set.pkl'
-sp_word_ngram_feature_set_path = '.\serialised_objects\sp_word_ngram_feature_set.pkl'
-sp_avg_sentence_length_feature_set_path = '.\serialised_objects\sp_avg_sentence_length_feature_set.pkl'
+sp_classifier_path = './serialised_objects/sp_classifier.pkl'
+sp_authors = './serialised_objects/sp_authors.pkl'
+sp_training_char_ngrams_path = './serialised_objects/sp_training_char_ngrams.pkl'
+sp_training_word_ngrams_path = './serialised_objects/sp_training_word_ngrams.pkl'
+sp_char_ngram_feature_set_path = './serialised_objects/sp_char_ngram_feature_set.pkl'
+sp_word_ngram_feature_set_path = './serialised_objects/sp_word_ngram_feature_set.pkl'
+sp_avg_sentence_length_feature_set_path = './serialised_objects/sp_avg_sentence_length_feature_set.pkl'
 
 # Constants
 SPANISH = -1
--- test_classifier.py
+++ test_classifier.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pickle
 import time
 from sys import argv
@@ -8,6 +9,75 @@ from sklearn.svm import SVC, LinearSVC
 
 import config
 from classifier.english_classifier import english_classifier
+
+
+def cv_classifier_score(classifier, train, target):
+    cv_score =  cross_val_score(classifier, train, target, cv=10)
+    return "Accuracy: %0.2f (+/- %0.2f)" % (cv_score.mean(), cv_score.std() * 2)
+
+
+def test_classifier_kernels(authors, features_normalized, features_text, language):
+    """
+    Creates a text file which outputs the results for english classifier kernel test.
+    - authors, the authors within the classifier, the target file
+    - features_normalized, normalized feature set to be used by the classifier
+    - features_text, text of what is included in the feature set being passed
+    """
+    curr_time = datetime.now().strftime("%Y-%m-%d_%Hh-%Mm-%Ss")
+    lang_name = 'unknown'
+
+    if language == config.ENGLISH:
+        lang_name = 'en'
+    elif language == config.SPANISH:
+        lang_name = 'sp'
+
+    output_file = open('./testing/' + lang_name + '_classifier_kernel_test_' + curr_time + '.txt', 'w+')
+    output_file.write("TEST: English Classifier Kernels " + curr_time + '\n')
+    output_file.write("Features:\n" + features_text +"\n")
+
+    print("TEST: English Classifier Kernels " + curr_time)
+    print("Features:\n" + features_text)
+
+    # Linear SVC Kernel
+    linear_svc = LinearSVC()
+    linear_svc_score = cv_classifier_score(linear_svc, features_normalized, authors)
+    output_file.write(linear_svc_score + '\n')
+    print(linear_svc_score)
+
+    # SVC Linear Kernel
+    svc_linear = SVC(kernel='linear')
+    svc_linear_score = cv_classifier_score(svc_linear, features_normalized, authors)
+    output_file.write(svc_linear_score + '\n')
+    print(svc_linear_score)
+
+    # SVC Poly Kernel
+    svc_poly = SVC(kernel='poly')
+    svc_poly_score = cv_classifier_score(svc_poly, features_normalized, authors)
+    output_file.write(svc_poly_score + '\n')
+    print(svc_poly_score)
+
+    # SVC rbf Kernel
+    svc_rbf = SVC(kernel='rbf')
+    svc_rbf_score = cv_classifier_score(svc_rbf, features_normalized, authors)
+    output_file.write(svc_rbf_score + '\n')
+    print(svc_rbf_score)
+
+    # SVC Sigmoid Kernel
+    svc_sigmoid = SVC(kernel='sigmoid')
+    svc_sigmoid_score = cv_classifier_score(svc_sigmoid, features_normalized, authors)
+    output_file.write(svc_sigmoid_score + '\n')
+    print(svc_sigmoid_score)
+
+    # SVC Precomputed Kernel
+    svc_precomputed = SVC(kernel='precomputed')
+    svc_precomputed_score = cv_classifier_score(svc_precomputed, features_normalized, authors)
+    output_file.write(svc_precomputed_score + '\n')
+    print(svc_precomputed_score)
+
+    output_file.close()
+
+    return
+
 
 if __name__ == '__main__':
     t1 = time.time()
@@ -40,18 +110,18 @@ if __name__ == '__main__':
             en_avg_sentence_length_feature_set = pickle.load(fid)
 
     # Loading Spanish Features
-    if language <= 0:
-        with open(config.sp_authors, 'rb') as fid:
-            sp_authors = pickle.load(fid)
+    # if language <= 0:
+    #     with open(config.sp_authors, 'rb') as fid:
+    #         sp_authors = pickle.load(fid)
 
-        with open(config.sp_char_ngram_feature_set_path, 'rb') as fid:
-            sp_char_ngram_feature_set = pickle.load(fid)
+    #     with open(config.sp_char_ngram_feature_set_path, 'rb') as fid:
+    #         sp_char_ngram_feature_set = pickle.load(fid)
 
-        with open(config.sp_word_ngram_feature_set_path, 'rb') as fid:
-            sp_word_ngram_feature_set = pickle.load(fid)
+    #     with open(config.sp_word_ngram_feature_set_path, 'rb') as fid:
+    #         sp_word_ngram_feature_set = pickle.load(fid)
 
-        with open(config.sp_avg_sentence_length_feature_set_path, 'rb') as fid:
-            sp_avg_sentence_length_feature_set = pickle.load(fid)
+    #     with open(config.sp_avg_sentence_length_feature_set_path, 'rb') as fid:
+    #         sp_avg_sentence_length_feature_set = pickle.load(fid)
 
     # FEATURE NORMALIZATION AND NORMALIZATION #
     t0 = time.time()
@@ -68,15 +138,15 @@ if __name__ == '__main__':
         en_training_feature_set_normalised = normalize(en_training_feature_set, norm=config.normalization_type)
 
     # Spanish Features
-    if language <= 0:
-        sp_training_feature_set = []
+    # if language <= 0:
+    #     sp_training_feature_set = []
 
-        # Assembling them such that they look like x
-        for i in range(len(sp_authors)):
-            sp_training_feature_set.append(
-                [sp_avg_sentence_length_feature_set[i]] + sp_char_ngram_feature_set[i] + sp_word_ngram_feature_set[i])
+    #     # Assembling them such that they look like x
+    #     for i in range(len(sp_authors)):
+    #         sp_training_feature_set.append(
+    #             [sp_avg_sentence_length_feature_set[i]] + sp_char_ngram_feature_set[i] + sp_word_ngram_feature_set[i])
 
-        sp_training_feature_set_normalised = normalize(sp_training_feature_set, norm=config.normalization_type)
+    #     sp_training_feature_set_normalised = normalize(sp_training_feature_set, norm=config.normalization_type)
 
     print("Zipping and Normalisation Time:", time.time() - t0)
     t0 = time.time()
